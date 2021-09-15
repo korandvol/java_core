57cd2b36adead9d503c8ac673ecbcc839eb5b23a
korandvol <korandvol@gmail.com>
2021-09-15 16:57:58 +0300

    oop - encapsulation

---
+++ 1/src/ru/javarush/Main.java
@@ -0,0 +1,12 @@
+package ru.javarush;
+
+public class Main {
+
+    public static void main(String[] args){
+        Computer computer = new Computer();
+        computer.setName("IBM");
+        computer.on();
+        computer.off();
+
+    }
+}
